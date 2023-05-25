8bc3e85b5c577ef221d1e23a750ec7d81a0d6754
CSandu <CristinaAlexandra.Sandu@harman.com>
2023-05-25 11:16:25 +0300

    Adding the WindowControlService class

---
+++ src/main/java/services/WindowControl/WindowControlService.java
@@ -0,0 +1,16 @@
+package services.WindowControl;
+
+import onlineservices.Service;
+
+public class WindowControlService implements Service {
+
+    @Override
+    public void onCreate() {
+
+    }
+
+    @Override
+    public void onDestroy() {
+
+    }
+}
